58e65688d60ea841b074e524955df6fb6a3d1a41
DazmaYT <dazmayt@example.com>
2026-06-17 14:34:31 +0500

    Update

--- img/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker: Установлен');
-});
-
-self.addEventListener('fetch', (event) => {
-  // Тут можно добавить кэширование, но для начала пусть будет пустым
-});
